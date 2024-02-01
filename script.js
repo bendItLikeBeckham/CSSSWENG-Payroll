2fb6911dcfa7fa880945a2da1634ca9efdfcee9e
Jesca Ryanne Acosta <jesca_ryanne_acosta@dlsu.edu.ph>
2024-02-01 17:36:26 +0800

    Added working current date

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 setInterval(()=>{
     const time = document.querySelector("#time");
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    const date = document.querySelector("#date");
+    let clock = new Date();
+    let hours = clock.getHours();
+    let minutes = clock.getMinutes();
+    let seconds = clock.getSeconds();
+
+    let day = clock.getDate();
+    let monthIndex = clock.getMonth();
+    let weekdayIndex = clock.getDay();
+    let year = clock.getFullYear();
+
     if(hours < 10){
         hours = "0"+ hours;
     }
@@ -14,5 +21,12 @@ setInterval(()=>{
         seconds = "0"+ seconds;
     }
 
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let month = months[monthIndex];
+    let weekday = weekdays[weekdayIndex];
+
     time.textContent = hours + ":" + minutes + ":" + seconds;
+    date.textContent = weekday + ", " + month + " " + day + ", " + year;
 })
