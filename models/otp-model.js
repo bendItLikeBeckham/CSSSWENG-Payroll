7ab7c5612156188c93c38c9833a209bb7c040bc4
Gabe005 <103907860+Gabe005@users.noreply.github.com>
2024-02-24 10:48:38 +0800

    Create otp-model.js

---
+++ models/otp-model.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var Current_otp = new mongoose.Schema({
+    current_otp: { 
+        type: Number,    
+    }
+});
+
+module.exports = mongoose.model('otp', Current_otp);
