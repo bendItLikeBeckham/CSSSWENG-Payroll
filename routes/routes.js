0f429527122990965db15b79cc4deaabb0c6ee06
Gabe005 <103907860+Gabe005@users.noreply.github.com>
2024-02-24 10:48:04 +0800

    Update routes.js
    otp routes added

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const login_controllers = require('../controllers/login-controller');
 const employee_dashboard_controllers = require('../controllers/employee-dashboard-controller');
 const employee_clockpage_controllers = require('../controllers/employee-clockpage-controller');
 const logout_controllers = require('../controllers/logout-controller');
+const otp_controller = require('../controllers/otp-controller')
 
 
 
@@ -25,5 +26,9 @@ app.get('/dummy_register', dummy_register_controllers.get_dummy_register);
 app.post('/dummy_register_employee', dummy_register_controllers.post_dummy_register);
 app.get('/employee_clockpage', employee_clockpage_controllers.get_employee_clockpage);
 app.get('/logout', logout_controllers.get_logout);
+app.post('/generate_otp', otp_controller.post_generate_otp);
+app.post('/verify_otp', otp_controller.post_verify_otp);
+
+
 
 module.exports = app;
