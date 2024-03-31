cbc07cbec92c512ad925f8e2808c031041539bf2
Are27 <jose_joaquin_arevalo@dlsu.edu.ph>
2024-03-31 20:16:15 +0800

    8:22

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const routes = require('./routes/routes.js');
 const hbs = require('hbs');
 const session = require('express-session');
 const database = require('./models/database.js');
-const schedule = require('node-schedule');
+
+const schedule = require('node-schedule');//might remove this
+// const cron = require('node-cron');
+// const axios = require('axios');
+
 const MongoStore = require('connect-mongo');
 
 const app = express();
@@ -45,7 +49,7 @@ app.use('/', routes);
 //not what happens to the fetch values when the payroll website is on render
 //what happens when the payroll is opened on monday would it not update the payroll since it is passed sunday 12am
 // schedule.scheduleJob('0 0 * * 0', function(){//uncomment this after checking
-schedule.scheduleJob('16 20 * * 0', function(){
+schedule.scheduleJob('22 20 * * 0', function(){
     //call to routes
     console.log("updating payroll!!");
 
