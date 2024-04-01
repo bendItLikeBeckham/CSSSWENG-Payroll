091de66bfec9981fa51b11569deede352dfbd05c
Are27 <jose_joaquin_arevalo@dlsu.edu.ph>
2024-04-01 23:40:26 +0800

    installed and implemeneted axios for the weekly payroll update to be similar to deployment branch

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/* 
+Functions:
+-This is the file that will be run during deployment or locally hosted
+-Session Creation and uploading to database
+-Call to connect to the MongoDB database
+-NPM packages utilized
+-Trigger update weekly payroll on PST: Sunday 12am/ UTC: Saturday 4pm
+-Redirect to login page
+*/
+
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
@@ -6,13 +16,14 @@ const hbs = require('hbs');
 const session = require('express-session');
 const database = require('./models/database.js');
 const schedule = require('node-schedule');
+const axios = require('axios');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 dotenv.config();
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
-
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -29,30 +40,27 @@ app.use(session({
     secret: 'session-secret-key', // Replace with your secret key
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://Admin_Acc:6rtztqN8cgcS6uwg@payrollcluster.ho2w0w9.mongodb.net/'
+    }),
     cookie: { 
-        httpOnly: true 
+        httpOnly: true, 
+        maxAge: 24 * 60 * 60 * 1000
     }
 }));
 
 app.use('/', routes);
 
-
-//not what happens to the fetch values when the payroll website is on render
-//what happens when the payroll is opened on monday would it not update the payroll since it is passed sunday 12am
-schedule.scheduleJob('0 0 * * 0', function(){
-    // schedule.scheduleJob('34 21 * * 0', function(){
-    //schedule.scheduleJob('*/3 * * * * *', function(){
-        //call to routes
-        console.log("updating payroll!!");
-    
-        //fetch("/update_employee_payroll");
-        fetch(`http://${hostname}:${port}/update_employee_payroll`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            }, 
+//PST: Sunday 12am/ UTC: Saturday 4pm
+schedule.scheduleJob('39 23 * * 1', function(){
+    axios.post(`http://${hostname}:${port}/update_employee_payroll`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error("Error Updating Payroll:", error);
         });
-    });
+});
 
 app.use(function(req, res){
     res.status(404).send('Error 404: Page Not Found');
